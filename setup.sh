6058e4100924845fbcfbf9aabb87df5ecf4cfbce
Julian Grosshauser <julian@juliangrosshauser.com>
2014-01-10 08:59:43 +0100

    Add simple setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ln -sf $PWD/.aliases ~/.aliases
+ln -sf $PWD/.functions ~/.functions
+ln -sf $PWD/.gitconfig ~/.gitconfig
+ln -sf $PWD/.slate ~/.slate
+ln -sf $PWD/.vimrc.after ~/.vimrc.after
+ln -sf $PWD/.zshenv ~/.zshenv
+ln -sf $PWD/.zshrc ~/.zshrc

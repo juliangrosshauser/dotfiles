93377c4347b0f7272819ab96cf74675419839312
Julian Grosshauser <julian@juliangrosshauser.com>
2014-01-20 10:20:28 +0100

    Update setup script
    Install dependencies, backup existing files and separate between OS X and Linux

--- setup.sh
+++ setup.sh
@@ -1,8 +1,43 @@
 #!/bin/bash
-ln -sf $PWD/.aliases ~/.aliases
-ln -sf $PWD/.functions ~/.functions
-ln -sf $PWD/.gitconfig ~/.gitconfig
-ln -sf $PWD/.slate ~/.slate
-ln -sf $PWD/.vimrc.after ~/.vimrc.after
-ln -sf $PWD/.zshenv ~/.zshenv
-ln -sf $PWD/.zshrc ~/.zshrc
+OH_MY_ZSH = ~/.oh-my-zsh
+JANUS = ~/.vim/janus
+
+# install dependencies
+if [ ! -d "$OH_MY_ZSH" ]; then
+  # install oh-my-zsh
+  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+fi
+
+if [ ! -d "$JANUS" ]; then
+  # install janus
+  curl -Lo- https://bit.ly/janus-bootstrap | bash
+fi
+
+# backup existing files
+for i in ~/.aliases ~/.functions ~/.vimrc.after ~/.zshrc; do
+  if [ -e $i ]; then
+    mv "${i}" "${i}.backup" || die "Could not move ${i} to ${i}.backup"
+    echo "${i} has been renamed to ${i}.backup"
+  fi
+done
+
+ln -s $PWD/.aliases ~/.aliases
+ln -s $PWD/.functions ~/.functions
+ln -s $PWD/.vimrc.after ~/.vimrc.after
+ln -s $PWD/.zshrc ~/.zshrc
+
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+  # backup existing files
+  for i in ~/.gitconfig ~/.slate ~/.gemrc ~/.zshenv ~/.zlogin; do
+    if [ -e $i ]; then
+      mv "${i}" "${i}.backup" || die "Could not move ${i} to ${i}.backup"
+      echo "${i} has been renamed to ${i}.backup"
+    fi
+  done
+
+  ln -s $PWD/.gitconfig ~/.gitconfig
+  ln -s $PWD/.slate ~/.slate
+  ln -s $PWD/.gemrc ~/.gemrc
+  ln -s $PWD/.zshenv ~/.zshenv
+  ln -s $PWD/.zlogin ~/.zlogin
+fi

e70c786b328cccd767d9769156b5dee7ec0d4656
Julian Grosshauser <julian@juliangrosshauser.com>
2014-01-20 10:18:53 +0100

    New OS X settings

--- osx.sh
+++ osx.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 # allow text selection Quick Look window
 defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# restart automatically if the computer freezes
+systemsetup -setrestartfreeze on
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1

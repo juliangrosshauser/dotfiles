ee919586c9a8b9893217f847a5b7d3ec5db286d1
Julian Grosshauser <julian@juliangrosshauser.com>
2014-09-11 20:57:53 +0200

    Update setup script
    Pure-prompt is now part of oh-my-zsh, so no reason to install it separatly
    Anymore. Remove .slate and .zlogin from setup process. Add success message and
    Exit code.

--- setup.sh
+++ setup.sh
@@ -13,11 +13,6 @@ if [ ! -d "$JANUS" ]; then
   curl -Lo- https://bit.ly/janus-bootstrap | bash
 fi
 
-if hash npm 2>/dev/null; then
-  npm install -g pure-prompt
-  ln -s /usr/local/lib/node_modules/pure-prompt/pure.zsh $OH_MY_ZSH/custom/pure.zsh-theme
-fi
-
 # backup existing files
 for i in ~/.aliases ~/.functions ~/.vimrc.after ~/.zshrc; do
   if [ -e $i ]; then
@@ -33,7 +28,7 @@ ln -s $PWD/.zshrc ~/.zshrc
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
   # backup existing files
-  for i in ~/.gitconfig ~/.slate ~/.gemrc ~/.zshenv ~/.zlogin; do
+  for i in ~/.gitconfig ~/.gemrc ~/.zshenv; do
     if [ -e $i ]; then
       mv "${i}" "${i}.backup" || die "Could not move ${i} to ${i}.backup"
       echo "${i} has been renamed to ${i}.backup"
@@ -41,8 +36,9 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
   done
 
   ln -s $PWD/.gitconfig ~/.gitconfig
-  ln -s $PWD/.slate ~/.slate
   ln -s $PWD/.gemrc ~/.gemrc
   ln -s $PWD/.zshenv ~/.zshenv
-  ln -s $PWD/.zlogin ~/.zlogin
 fi
+
+echo "dotfiles setup complete"
+exit 0
